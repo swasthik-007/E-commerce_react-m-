6f84874623750160435ebe6973847091726083aa
swasthik-007 <mohanty.swastik4@gmail.com>
2024-05-25 12:39:56 +0530

    Added basic routing using react-router

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,13 @@ import './Home.css';
 
 function Home() {
     return (
-        <div className="container">
+        <div className="container welcome-wrapper">
             <div className="row">
                 <h2 className="home-title text-center">Welcome to Shop Cart</h2>
                 <div className="category-list d-flex flex-row justify-content-between align-items-center" id="categoryList">
-
+                    
                     <CategoryItem itemName="All Products" />
-
+                    
                 </div>
                 <div className="category-title text-center">
                     Select a category to start Shopping
@@ -19,5 +19,4 @@ function Home() {
         </div>
     );
 }
-
 export default Home;
