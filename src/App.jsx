e6990db44ab4c6ba476eadabb3f0fe43f2dea194
swasthik-007 <mohanty.swastik4@gmail.com>
2024-05-25 11:03:56 +0530

    added footer and removed sticky navbar to normal navbar Reference footer: https://getbootstrap.com/docs/5.3/examples/sticky-footer-navbar/

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 function App() {
 
   return (
-    <>
-      <Header color="light" light={true} expand="md" container="md" fixed="top" />
-    </>
+    <div className="app-wrapper">
+      <Header color="light" light={true} expand="md" container="md"/>
+      <div>
+        Some content
+      </div>
+      <Footer />
+    </div>
   )
 }
+
 export default App
