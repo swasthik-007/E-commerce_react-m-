fe10bba0c3c821c0357138ceb1ad91ef2c06cd1d
swasthik-007 <mohanty.swastik4@gmail.com>
2024-05-25 22:51:45 +0530

    Added product page redirection to details

--- src/apis/fakeStoreProdApis.js
+++ src/apis/fakeStoreProdApis.js
@@ -1,11 +1,14 @@
 export function getAllCategories() {
     return 'https://fakestoreapi.com/products/categories';
 }
-
 export function getAllProducts() {
     return 'https://fakestoreapi.com/products';
 }
 
 export function getAllProductsByCategory(category) {
     return `https://fakestoreapi.com/products/category/${category}`;
+}
+
+export function getProduct(id) {
+    return `https://fakestoreapi.com/products/${id}`;
 }
--- src/pages/ProductList/ProductList.jsx
+++ src/pages/ProductList/ProductList.jsx
@@ -1,33 +1,24 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useSearchParams } from 'react-router-dom';
-
 // CSS import
 import './ProductList.css';
-
 // Component import
 import ProductBox from '../../components/ProductBox/ProductBox';
 import FilterProducts from '../../components/FilterProducts/FilterProducts';
-
 import { getAllProducts, getAllProductsByCategory } from '../../apis/fakeStoreProdApis';
-
-
 function ProductList() {
-
     const [productList, setProductList] = useState(null);
     const [query] = useSearchParams();
-
     async function downloadProducts(category) {
         const downloadUrl = category ? getAllProductsByCategory(category) : getAllProducts();
         const response = await axios.get(downloadUrl);
         setProductList(response.data);
         console.log(response.data);
     }
-
     useEffect(() => {
         downloadProducts(query.get("category"));
-    }, [])
-
+    }, [query.get("category")])
     return (
         <div className='container'>
             <div className='row'>
@@ -36,8 +27,10 @@ function ProductList() {
                     <FilterProducts />
                     {/* list of products */}
                     <div className='product-list-box' id='productList'>
+
                         {productList && productList.map(
                             (product) => <ProductBox 
+                                            productId={product.id}
                                             key={product.id} 
                                             name={product.title}
                                             price={product.price}
