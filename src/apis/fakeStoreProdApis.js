0da92ce2c5808d74cf942fbae212b410db638c5c
swasthik-007 <mohanty.swastik4@gmail.com>
2024-05-26 13:48:59 +0530

    added SignUp for the app

--- src/apis/fakeStoreProdApis.js
+++ src/apis/fakeStoreProdApis.js
@@ -1,15 +1,17 @@
 export function getAllCategories() {
     return `${import.meta.env.VITE_FAKE_STORE_URL}/products/categories`;
 }
-
 export function getAllProducts() {
     return `${import.meta.env.VITE_FAKE_STORE_URL}/products`;
 }
-
 export function getAllProductsByCategory(category) {
     return `${import.meta.env.VITE_FAKE_STORE_URL}/products/category/${category}`;
 }
 
 export function getProduct(id) {
     return `${import.meta.env.VITE_FAKE_STORE_URL}/products/${id}`;
+}
+
+export function signup() {
+    return `${import.meta.env.VITE_FAKE_STORE_URL}/users`;
 }
